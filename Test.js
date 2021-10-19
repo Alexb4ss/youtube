e49e2d573816a1807f1d4d3745628b8bf05ddb5f
Alexb4ss <92802193+Alexb4ss@users.noreply.github.com>
2021-10-19 15:15:20 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,3 +1,8 @@
 window.addEventListener("load", function() {
     alert('Page is loaded');
 });
+
+window.onload = function exampleFunction() {
+  alert('Page is loaded 2'); 
+    // Function to be executed
+}
